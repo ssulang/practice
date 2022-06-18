63d180210a4bb1950cfd486a3565fab5dee17a8b
ssulang <terry940513@naver.com>
2022-06-18 19:04:31 +0900

    0618

---
+++ ch1/src/ch2/문자열을숫자로.java
@@ -0,0 +1,17 @@
+package ch2;
+
+public class 문자열을숫자로 {
+
+	public static void main(String[] args) {
+		
+		//int seven = "7";
+		int seven = Integer.parseInt("7");
+		
+		//double pi = "3.14";
+		double pi = Double.parseDouble("3.14");
+				
+		System.out.println("seven =" + seven);
+		System.out.println("pi = " + pi);
+	}
+
+}
---
+++ ch1/src/ch2/타입불일치.java
@@ -0,0 +1,16 @@
+package ch2;
+
+public class 타입불일치 {
+
+	public static void main(String[] args) {
+		//String five = 5;
+		
+		//int five = 5;
+		
+		String five = "" + 5;
+		
+		System.out.println(five);
+
+	}
+
+}
