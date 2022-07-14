763dbefb40c13d0b90d29bb91de5569518b02f7d
ssulang <terry940513@naver.com>
2022-07-14 18:07:26 +0900

    0714

---
+++ ch1/src/ch3/WhileTest.java
@@ -0,0 +1,23 @@
+package ch3;
+
+public class WhileTest {
+
+	public static void main(String[] args) {
+
+		int num = 1;
+		int sum  = 0;
+		
+		while( num <= 10) {
+			
+			sum += num;
+			num++;
+		}
+		
+		System.out.println(sum);
+		System.out.println(num);
+	}
+
+} // sum = 0 + 1
+  // sum = 1 + 2 
+  // sum = 3 + 3 ...
+  // sum = 45 + 10
