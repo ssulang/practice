576d4b146617f145b4b46b13639331a476a6f4fe
ssulang <terry940513@naver.com>
2022-07-17 16:31:39 +0900

    0716

---
+++ ch1/src/ch4/StudentTest.java
@@ -0,0 +1,24 @@
+package ch4;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		//Student란 클래스 불러오기
+		Student studentLee = new Student();
+		studentLee.studentName = "이순신";
+		studentLee.address = "서울";
+		
+		
+		studentLee.showStudentInfo(); //System.out.println(studentName + "," + address);
+		
+		Student studentKim = new Student();
+		studentKim.studentName = "김유신";
+		studentKim.address = "경주";
+		
+		studentKim.showStudentInfo();
+		
+		System.out.println(studentLee);
+		System.out.println(studentKim);
+	}
+
+}
